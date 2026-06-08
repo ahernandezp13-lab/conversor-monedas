0e982a1f21f32fd3ef6b4150c5abbc84f597659a
ahernandezp13-lab <ahernandezp13@unicartagena.edu.co>
2026-06-08 18:41:47 -0500

    Actualizacion errores de capa 8

--- src/ConversorMonedasCli.java
+++ src/ConversorMonedasCli.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class ConversorMonedasCli {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void iniciar() {
+
+        System.out.println("=== CONVERSOR DE MONEDAS ===");
+
+        System.out.print("Ingrese la cantidad en USD: ");
+        double usd = scanner.nextDouble();
+
+        double tasaCop = 4000; // Ejemplo
+        double cop = usd * tasaCop;
+
+        System.out.println("Resultado:");
+        System.out.println(usd + " USD = " + cop + " COP");
+    }
 }
